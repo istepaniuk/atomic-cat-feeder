5149d25f586b052126ad1a9f7f76fe12651fb3cd
Iván Stepaniuk <istepaniuk@gmail.com>
2017-11-20 01:31:12 +0100

    better signal sampling
    synchronizing and use a timer to capture the signal value in the
    "1" region instead of relying on edge detection which is very
    suceptible to noise.

--- src/dcf77.c
+++ src/dcf77.c
@@ -3,65 +3,83 @@
 #include "dcf77.h"
 #include "hardware.h"
 #include "usart.h"
-#include "delay.h"
+#include "timer.h"
+#include "interrupts.h"
 
-const int SAMPLE_SIZE = 8;
+static const int SAMPLE_SIZE = 64;
 
-int get_average_signal_value(int samples);
+int get_average_signal_value(int samples)
+{
+    int high_sample_count = 0;
+    for (int i = 0; i < SAMPLE_SIZE; i++) {
+        high_sample_count += (samples >> i) & 1;
+    }
+
+    return high_sample_count > 32;
+}
+
+static int ticks = 0;
+static int its1 = 0;
+
+void timer2_interrupt_handler()
+{
+    ticks++;
+    if(ticks >= 100 && ticks < 200){
+        if(gpio_get_pin_state(&DCF77_IN_PIN)){
+            its1++;
+        }
+    } else if(ticks >= 1000){
+        if(its1 > 50){
+            usart_putc('1');
+        } else {
+            usart_putc('0');
+        }
+        its1 = 0;
+        ticks = 0;
+    }
+}
+
 void setup_dcf77()
 {
     gpio_set_pin_mode(&DCF77_IN_PIN, GPIO_MODE_IN_FLOATING);
     gpio_set_pin_mode(&DCF77_POWER_PIN, GPIO_MODE_OUT_PUSH_PULL);
     gpio_set_pin_high(&DCF77_POWER_PIN);
 
-    usart_puts("DCF\n");
-
     int signal = 0;
     int samples = 0;
     int sample_index = 0;
     int previous_reading = 0;
     int signal_high_time = 0;
-    int signal_low_time = 0;
+
+    //Update_event = TIM_CLK/((PSC + 1)*(ARR + 1))
+    timer2_init(1000-1, 24-1);
+    timer2_stop();
+    set_timer2_interrupt_callback(timer2_interrupt_handler);
+
+    usart_puts("Waiting for DCF minute mark...\n");
 
     while (1) {
         bool pin_state = !gpio_get_pin_state(&DCF77_IN_PIN);
-        samples =
-            (samples & (~(1 << sample_index))) | (pin_state <<
-                                                  sample_index);
+        samples = (samples & (~(1 << sample_index))) | (pin_state << sample_index);
         if (++sample_index >= SAMPLE_SIZE) {
             signal = get_average_signal_value(samples);
             sample_index = 0;
         }
-        //raising edge
-        if (signal > previous_reading) {
-            signal_high_time = 0;
 
-            if (signal_low_time > 180 && signal_low_time < 220) {
-                usart_putc('1');
-            } else if (signal_low_time > 80 && signal_low_time < 120) {
-                usart_putc('0');
-            } else {
-                usart_putc('?');
-                usart_putc(' ');
-                printint(signal_low_time);
-                usart_putc(' ');
-            }
-
-        }
         //falling edge
         if (signal < previous_reading) {
-            signal_low_time = 0;
-            if (signal_high_time > 1000) {
-                usart_putc('\n');
+            if (signal_high_time > 60000) {
+                timer2_start();
+                usart_puts("\nM ");
             }
+            signal_high_time = 0;
         }
 
         if (signal) {
+            signal_high_time++;
             gpio_set_pin_high(&BLUE_LED_PIN);
-            signal_high_time += 1;
         } else {
             gpio_set_pin_low(&BLUE_LED_PIN);
-            signal_low_time += 1;
         }
 
         if (gpio_get_pin_state(&DCF77_IN_PIN)) {
@@ -71,16 +89,5 @@ void setup_dcf77()
         }
 
         previous_reading = signal;
-        delay_ms(1);
     }
-}
-
-int get_average_signal_value(int samples)
-{
-    int high_sample_count = 0;
-    for (int i = 0; i < SAMPLE_SIZE; i++) {
-        high_sample_count += (samples >> i) & 1;
-    }
-
-    return high_sample_count > 4;
 }
--- src/interrupts.h
+++ src/interrupts.h
@@ -3,6 +3,7 @@
 
 #include <stm32f10x.h>
 
+
 void set_exti_line_interrupt_callback(int line, void *callback);
 void set_timer2_interrupt_callback(void *callback);
 
